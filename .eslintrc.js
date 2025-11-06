e988af25d4dc922b8bac3277cc8f10c0507717df
Sakeeb91 <rahman.sakeeb@gmail.com>
2025-11-06 09:10:28 -0500

    chore: configure eslint baseline (refs #1)

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
+      },
+    ],
+  },
+  ignorePatterns: ['dist', 'build', 'node_modules', '.turbo'],
+};
