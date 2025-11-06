0d536a8fa08bd3cc72786cf491ada93e4b81cab6
Sakeeb91 <rahman.sakeeb@gmail.com>
2025-11-06 09:57:21 -0500

    chore: tune eslint for node commonjs modules (refs #15)

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,14 @@ module.exports = {
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es2020: true,
+  },
   plugins: ['@typescript-eslint', 'import'],
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
@@ -24,4 +32,19 @@ module.exports = {
     ],
   },
   ignorePatterns: ['dist', 'build', 'node_modules', '.turbo'],
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+    {
+      files: ['**/*.test.js'],
+      env: {
+        node: true,
+        es2020: true,
+      },
+    },
+  ],
 };
